195b53ffbed4cc8df4be28735cfa2e01f9e3d8dd
NamNguyen911 <mr.nam911@gmail.com>
2017-05-01 22:32:53 +0700

    add description and price for products

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170501144728) do
+ActiveRecord::Schema.define(version: 20170501152501) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
+    t.decimal  "price"
   end
 
 end
