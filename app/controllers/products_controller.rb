2195b4143c8d733187f34e52fb2decc1b7f4a6cd
NamNguyen911 <mr.nam911@gmail.com>
2017-05-07 18:11:46 +0700

    load and authorize product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,11 @@
 class ProductsController < ApplicationController
+  load_and_authorize_resource
+
   def index
     @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
   end
 end
