a9c64c099e9df74ec5f17239ba726c8a0608f29c
NamNguyen911 <mr.nam911@gmail.com>
2017-05-21 19:55:16 +0700

    handle can can exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,8 @@ class ApplicationController < ActionController::Base
       Order.new
     end
   end
+
+  rescue_from CanCan::AccessDenied do |exception|
+    render :file => "#{Rails.root}/public/403.html", :status => 403, :layout => false
+  end
 end
