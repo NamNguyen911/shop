7850300825f16bc00611d183e61f3c08417e2abc
NamNguyen911 <mr.nam911@gmail.com>
2017-05-29 12:51:17 +0700

    using locale to manage vietnamese and english content

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method [:current_order, :categories]
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+    # I18n.locale = :en
+  end
 
   def categories
     Category.all
