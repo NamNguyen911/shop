1f9b385e9646db311569fd182d1fbc92005f3633
NamNguyen911 <mr.nam911@gmail.com>
2017-06-12 09:35:17 +0700

    update default images for category and product

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   has_many :products
 
-  has_attached_file :photo, styles: { medium: "500x667>", thumb: "100x134>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :photo, styles: { medium: "500x667>", thumb: "100x134>" }, default_url: "/categories/:style/missing.jpg"
   validates_attachment_content_type :photo, content_type: /\Aimage\/.*\z/
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,6 @@
 class Image < ApplicationRecord
   belongs_to :product
 
-  has_attached_file :file, styles: { medium: "500x667>", thumb: "100x134>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :file, styles: { medium: "500x667>", thumb: "100x134>" }, default_url: "/images/:style/missing.jpg"
   validates_attachment_content_type :file, content_type: /\Aimage\/.*\z/
 end
