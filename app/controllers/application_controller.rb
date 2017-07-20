2a43c80f56fd3e28388d24fe8b229b591dd94f2a
NamNguyen911 <mr.nam911@gmail.com>
2017-07-20 11:56:35 +0700

    add form login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
   helper_method [:current_order, :categories]
   before_action :set_locale
 
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
-    # I18n.locale = :en
+    # I18n.locale = params[:locale] || I18n.default_locale
+    I18n.locale = :en
   end
 
   def categories
@@ -22,5 +23,11 @@ class ApplicationController < ActionController::Base
 
   rescue_from CanCan::AccessDenied do |exception|
     render :file => "#{Rails.root}/public/403.html", :status => 403, :layout => false
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in) do |user_params|
+      user_params.permit(:email, :password, :remember_me)
+    end
   end
 end
